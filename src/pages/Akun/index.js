f7c36dca5cab3bfdec16a2a18fea0263c4e3f61c
Rafel <rafelinoxz7@gmail.com>
2020-11-13 16:44:10 +0800

    flash screen

---
+++ src/pages/Akun/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Akun = () => {
+   return (
+      <View>
+         <Text>Akunmu</Text>
+      </View>
+   )
+}
+
+export default Akun
+
+const styles = StyleSheet.create({})
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Home, Akun, Pesanan, Splash} from '../pages';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+   return (
+       <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Pesanan" component={Pesanan} />
+        <Tab.Screen name="Akun" component={Akun} />
+      </Tab.Navigator>
+   )
+}
+
+
+const Router = () => {
+   return (
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false}} />
+        <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false}} />
+        {/* <Stack.Screen name="Pesanan" component={Pesanan} options={{ headerShown: false}} />
+        <Stack.Screen name="Akun" component={Akun} options={{ headerShown: false}} /> */}
+      </Stack.Navigator>
+   )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
